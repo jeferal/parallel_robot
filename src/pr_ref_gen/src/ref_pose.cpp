e4c5a335337587d6c74ba4d87d37837bcf54747a
Jesús Ferrándiz <jeferal@etsid.upv.es>
2020-12-07 18:53:24 +0100

    Compiled ref pose

--- src/pr_ref_gen/src/ref_pose.cpp
+++ src/pr_ref_gen/src/ref_pose.cpp
@@ -26,7 +26,7 @@ namespace pr_ref_gen
     {
         //Parameter declaration
         this->declare_parameter<std::string>("ref_path", 
-            "/home/paralelo4dofnew/aux_ws/referencias/ref_cart_TRR2_v2_CF1.txt");
+            "/home/paralelo4dofnew/parallel_robot_ws/references/ref_cart_TRR0_CF1_IdV1.txt");
         
         this->declare_parameter<bool>("is_cart", false);
         this->declare_parameter<std::vector<double>>("robot_config_params", 
@@ -98,3 +98,10 @@ namespace pr_ref_gen
     }
 
 }
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(pr_ref_gen::RefPose)
