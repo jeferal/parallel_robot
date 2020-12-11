891d3ad47c073b588de3dc4ab108c440682fdd96
Jesús Ferrándiz <jeferrandiz98@gmail.com>
2020-12-11 12:57:16 +0100

    created jac_dep component, fix conversions

---
+++ src/pr_modelling/include/pr_modelling/jac_dep.hpp
@@ -0,0 +1,47 @@
+#ifndef PR_MODELLING__JAC_DEP_HPP_
+#define PR_MODELLING__JAC_DEP_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "message_filters/subscriber.h"
+#include "message_filters/time_synchronizer.h"
+#include "message_filters/sync_policies/approximate_time.h"
+
+#include "pr_msgs/msg/pr_array_h.hpp"
+#include "pr_msgs/msg/pr_mat_h.hpp"
+#include "pr_lib/pr_model.hpp"
+#include "pr_lib/pr_utils.hpp"
+
+#include "eigen3/Eigen/Dense"
+
+namespace pr_modelling
+{
+    class DependentJacobian : public rclcpp::Node
+    {
+        public:
+            //PR_MODELLING_PUBLIC
+            explicit DependentJacobian(const rclcpp::NodeOptions & options);
+
+        protected:
+            void topic_callback(const pr_msgs::msg::PRArrayH::ConstPtr& x_coord_msg,
+                                const pr_msgs::msg::PRArrayH::ConstPtr& q_msg);
+
+        private:
+            message_filters::Subscriber<pr_msgs::msg::PRArrayH> sub_x;
+            message_filters::Subscriber<pr_msgs::msg::PRArrayH> sub_q;
+
+            typedef message_filters::sync_policies::ApproximateTime
+                    <pr_msgs::msg::PRArrayH, pr_msgs::msg::PRArrayH, pr_msgs::msg::PRArrayH> SyncPolicy;
+
+            typedef message_filters::Synchronizer<SyncPolicy> Synchronizer;
+            std::shared_ptr<Synchronizer> sync_;
+
+            rclcpp::Publisher<pr_msgs::msg::PRMatH>::SharedPtr publisher_;
+
+            std::vector<double> robot_params;
+            Eigen::Matrix<double, 11, 11> DepJ;
+            Eigen::Matrix<double, 4, 3> Q;
+    };
+}
+
+#endif // PR_MODELLING__JAC_DEP_HPP_
---
+++ src/pr_modelling/src/jac_dep.cpp
@@ -0,0 +1,66 @@
+#include "pr_modelling/jac_dep.hpp"
+
+#include <chrono>
+#include <memory>
+#include <utility>
+#include <array>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/qos.hpp"
+
+#include "pr_msgs/msg/pr_array_h.hpp"
+#include "pr_msgs/msg/pr_mat_h.hpp"
+
+#include "pr_lib/pr_model.hpp"
+#include "pr_lib/pr_utils.hpp"
+
+using std::placeholders::_1;
+
+namespace pr_modelling
+{
+    /**** INVERSE KINEMATICS COMPONENT ****/
+    DependentJacobian::DependentJacobian(const rclcpp::NodeOptions & options)
+    : Node("jac_dep", options)
+    {
+        //Parameter declaration
+        this->declare_parameter<std::vector<double>>(
+            "robot_config_params", 
+            {0.4, 0.4, 0.4, 0.15, 90*(M_PI/180), 45*(M_PI/180), 0.3, 0.3, 0.3, 50*(M_PI/180), 90*(M_PI/180)});
+        
+        this->get_parameter("robot_config_params", robot_params);
+
+        sub_x.subscribe(this, "x_coord");
+        sub_q.subscribe(this, "q_sol");
+        sync_.reset(new Synchronizer(SyncPolicy(1), sub_x, sub_q));
+        sync_->registerCallback(std::bind(&DependentJacobian::topic_callback, this, std::placeholders::_1, std::placeholders::_2));
+        sync_->setMaxIntervalDuration(rclcpp::Duration(0, 10000000));
+
+        publisher_ = this->create_publisher<pr_msgs::msg::PRMatH>("jac_dep", 1);
+    }
+
+    void DependentJacobian::topic_callback(const pr_msgs::msg::PRArrayH::ConstPtr& x_coord_msg,
+                                           const pr_msgs::msg::PRArrayH::ConstPtr& q_msg)
+    {
+        auto jac_dep_msg = pr_msgs::msg::PRMatH();
+
+        for(int i=0; i<q_msg->data.size(); i++){
+            Q(0,0) = 2;
+        }
+        
+        PRModel::DepJacobian(DepJ, Q, x_coord_msg->data[2], x_coord_msg->data[3], robot_params);
+
+        PRUtils::Eigen2Mat(DepJ, jac_dep_msg);
+
+        jac_dep_msg.header.stamp = this->get_clock()->now();
+
+        publisher_->publish(jac_dep_msg);
+    }
+}
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(pr_modelling::DependentJacobian)
