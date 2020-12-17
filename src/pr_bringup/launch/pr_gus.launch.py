70a1bc01dc05d37ed9626c2c4b7cbf7cc11a050f
Jesús Ferrándiz <jeferrandiz98@gmail.com>
2020-12-17 10:51:06 +0100

    need to check library py

--- src/pr_bringup/launch/pr_gus.launch.py
+++ src/pr_bringup/launch/pr_gus.launch.py
@@ -1,17 +1,58 @@
+import os
 import launch
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
+from ament_index_python.packages import get_package_share_directory
 
 from numpy import fromstring, pi
+
+def LoadConfiguration(config=1):
+
+    param_list = [
+        #R1    R2    R3    ds     betaFD     betaFI    Rm1   Rm2   Rm3    betaMD     betaMI
+        #config 0
+        [0.4 , 0.4 , 0.4 , 0.0 , 50*pi/180, 40*pi/180, 0.2 , 0.2 , 0.2 , 30*pi/180, 40*pi/180],
+        #config 1
+        [0.4 , 0.4 , 0.4 , 0.15, 90*pi/180, 45*pi/180, 0.3 , 0.3 , 0.3 , 50*pi/180, 90*pi/180],
+        #config 2
+        [0.4 , 0.4 , 0.4 , 0.15, 90*pi/180, 45*pi/180, 0.25, 0.25, 0.25, 5*pi/180 , 90*pi/180],
+        #config 3
+        [0.3 , 0.3 , 0.3 , 0.0 , 5*pi/180 , 90*pi/180, 0.2 , 0.2 , 0.2 , 70*pi/180, 30*pi/180],
+        #config 4
+        [0.4 , 0.4 , 0.4 , 0.15, 90*pi/180, 5*pi/180 , 0.15, 0.15, 0.15, 45*pi/180, 90*pi/180],
+        #config 5
+        [0.3 , 0.3 , 0.3 , 0.15, 50*pi/180, 90*pi/180, 0.15, 0.15, 0.15, 90*pi/180, 45*pi/180],
+        #config 6
+        [0.25, 0.25, 0.25, 0.15, 5*pi/180 , 90*pi/180, 0.15, 0.15, 0.15, 80*pi/180, 75*pi/180],
+        #config 7
+        [0.35, 0.35, 0.35, 0.15, 90*pi/180, 25*pi/180, 0.2 , 0.2 , 0.2 , 60*pi/180, 90*pi/180],
+        #congig 8
+        [0.35, 0.35, 0.35, 0.15, 35*pi/180, 30*pi/180, 0.15, 0.15, 0.15, 5*pi/180 , 90*pi/180]
+    ]
+
+    return param_list[config]
 
 def generate_launch_description():
 
     """Generate launch description with multiple components."""
 
+    #Load config file
+    """
+    config = os.path.join(
+        get_package_share_directory('pr_bringup'),
+        'config',
+        'pr_gus.yaml'
+    )
+    """
+
     ref_file = "/home/paralelo4dofnew/parallel_robot_ws/references/ref_qinde_TRR0_CF1_IdV1.txt"
     
     with open(ref_file, 'r') as f:
         first_reference = fromstring(f.readline(), dtype=float, sep=" ").tolist()
+
+    robot_config_params = LoadConfiguration(config=3)
+
+    print(robot_config_params)
 
     pr_gus = ComposableNodeContainer(
             node_name='pr_container',
@@ -113,7 +154,7 @@ def generate_launch_description():
                     parameters=[
                         {"ref_path": ref_file},
                         {"is_cart": False},
-                        {"robot_config_params": [0.4, 0.4, 0.4, 0.15, 90*(pi/180), 45*(pi/180), 0.3, 0.3, 0.3, 50*(pi/180), 90*(pi/180)]}
+                        {"robot_config_params": robot_config_params}
                     ]
                 ),
                 ComposableNode(
