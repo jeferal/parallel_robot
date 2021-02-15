6b69250c652d706c9fb46916100ee7d2a0a19c6a
Jesús Ferrándiz <jeferrandiz98@gmail.com>
2021-02-15 12:42:37 +0100

    created pr_rl pkg

---
+++ src/pr_rl/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'pr_rl'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='paralelo4dofnew',
+    maintainer_email='jeferrandiz98@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
